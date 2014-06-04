a2cdc7727d6a1bcc48cf17c3e5782372ae884448
Andy <acromace@gmail.com>
2014-06-04 01:25:22 -0400

    Added lowercase letters to alphaNumToMorse
    and removed the use of uppercaseString which seems to be causing an
    exception right now. Will revert to normal after that is fixed.

--- SwiftMorseCode/ViewController.swift
+++ SwiftMorseCode/ViewController.swift
@@ -37,6 +37,32 @@ class ViewController: UIViewController {
         "X": "-..-",
         "Y": "-.--",
         "Z": "--..",
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--..",
         "1": ".----",
         "2": "..---",
         "3": "...--",
@@ -58,7 +84,8 @@ class ViewController: UIViewController {
     }
     
     func convertStringToMorse(input: String) -> String {
-        let stringToConvert = input.uppercaseString
+        let stringToConvert = input
+//        let stringToConvert = input.uppercaseString
         var returnString = ""
         for char in stringToConvert {
             let returnChar = convertLetterToMorse(char)
