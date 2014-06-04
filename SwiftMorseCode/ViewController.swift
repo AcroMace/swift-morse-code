ff7cbcf503a9d23841f02af56b1413585258c9f0
Andy <acromace@gmail.com>
2014-06-03 22:56:40 -0400

    Initial Commit

---
+++ SwiftMorseCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMorseCode
+//
+//  Created by Andy Cho on 2014-06-03.
+//  Copyright (c) 2014 Andy Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
